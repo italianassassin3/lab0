9c3bdfd2b9264ace4ec82a948b7fe48669ec4c45
italianassassin3 <jose.frontanez@upr.edu>
2018-02-19 21:53:57 -0400

    Added permiter feature

--- areaCalculator/area.java
+++ areaCalculator/area.java
@@ -4,11 +4,20 @@ public class area{
 
 public double calcArea(int r){
 double a = Math.PI*(Math.pow(r, 2));
-System.out.println("Area of the circle with radius " + r +" is: ");
+System.out.println("Full Area of the circle with radius " + r +" is: ");
 
 System.out.printf("%.2f", a);
 
 System.out.println(" ");
+
+System.out.println("Half Area of the circle with radius " + r +" is: ");
+
+double half = a / 2;
+System.out.printf("%.2f", half);
+
+System.out.println(" ");
+
+
 return a;
 
 }
